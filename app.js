8fd06a35a1d7790d427d7d55af30fb67b5d26c43
J.Alberto Azevedo <alazib7@gmail.com>
2023-03-01 09:54:46 +0100

    dinamic router

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(cors())
 
 const port = process.env.PORT || 3000
 
+// We invoke routes here
+app.use("/api", require("./routes"))
+
 app.listen(port, () => {
   console.log("Tu app está lista por http://localhost:" + port)
 })
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const fs = require("fs")
+const router = express.Router()
+
+const PATH_ROUTES = __dirname
+
+const removeJsExtension = (fileName) => {
+  return fileName.split(".").shift()
+}
+fs.readdirSync(PATH_ROUTES).filter((file) => {
+  const name = removeJsExtension(file)
+  if (name !== "index") {
+    console.log(`Cargando ruta ${name}`)
+    router.use(`/${name}`, require(`./${file}`))
+  }
+})
+
+module.exports = router
